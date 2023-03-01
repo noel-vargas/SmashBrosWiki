178282cd15e598191b11e0e5edb12e4647acea9a
Noel Vargas <noel_vargas@techexchange.in>
2023-03-01 02:53:34 +0000

    Implemented 3 routes

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -6,34 +6,30 @@ class Backend:
     # when the class is created, should it have a parameter or nothing?
     # chan I choose any name for the bucket?
     def __init__(self):
-        self.storage_client = storage.Client()
-        self.bucket_name = "Smash-Characters"
+        self.storage_client = storage.Client("sds-project-nbs-wiki")
+        self.bucket_name = "nbs-wiki-content"
         # Instantiates a cliesnt
         self.bucket = self.storage_client.create_bucket(self.bucket_name)
-        # add the characters to the bucket
-        pages_folder = os.path.join(os.path.dirname(__file__), "pages")
-        files_in_folder = os.listdir(pages_folder)
-
-        #go through the files and create a blob for each one and upload it. 
-        for file in files_in_folder:
-            blob = self.bucket.blob(file)
-            with open(os.path.join(pages_folder,file), "rb") as f:
-                blob.upload_from_file(f)
-        
-        pass
         
     def get_wiki_page(self, name):
         # get the blob with the given name O(log(n))
         blob = self.bucket.blob(name)
 
+        # check if the blob exists
+        if not blob.exists():
+            return None
+
         blob_content = blob.download_as_string().decode("utf-8")
         return blob_content
+
 
     def get_all_page_names(self):
         # get a list of all the blobs in the bucket
         blobs = self.bucket.list_blobs()
         page_names = []
 
+        if len(blobs) ==0:
+            return None
         for blob in blobs:
             page_names.append(blob.name)
 
--- flaskr/pages.py
+++ flaskr/pages.py
@@ -14,5 +14,20 @@ def make_endpoints(app):
         # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
         # to render main.html on the home page.
         return render_template("main.html")
+
+    # CHECK HOW TO GET THE NAME IN HERE
+    @app.route("nbs-wiki-content/smash-characters", methods=["GET"])
+    def get_wiki_page(name):
+        page = backend.get_wiki_page(name)
+
+    @app.route("nbs-wiki-content/smash-characters", methods=["GET"])
+    def get_all_page_names():
+        page_names = backend.get_all_page_names()
+    
+
+    @app.route("nbs-wiki-content/smash-characters", methods=["GET"])
+    def upload(file_path, file_name):
+        backend.upload(file_path,file_name)
+
     
     # TODO(Project 1): Implement additional routes according to the project requirements.
