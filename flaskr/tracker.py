0b76fd693e79e11ad65e42358586036aadda3c75
Sebastian Maldonado-Rosado <sebastian.maldonado@techexchange.in>
2023-04-13 22:24:40 +0000

    Change add_page to use only one transaction. Implement comment methods

--- flaskr/tracker.py
+++ flaskr/tracker.py
@@ -1,4 +1,5 @@
 from google.cloud import datastore
+import json
 from unittest.mock import MagicMock
 
 
@@ -24,8 +25,7 @@ class Tracker:
 
     def add_upload(self,
                    username: str,
-                   pagename: str,
-                   user_uploads=None) -> None:
+                   pagename: str) -> None:
         """
         Keeps track of which user uploaded which pages, and the uploader
         for each page.
@@ -37,12 +37,8 @@ class Tracker:
             pagename:
                 String containing the name of uploaded page.
         """
-        # Add page to `uploads` array of `username` in UserUploads.
-        if isinstance(user_uploads, MagicMock):
-            before = []
-            after = before + [pagename]
-            return before, after
         with self.client.transaction() as trans:
+            # Add page to `uploads` array of `username` in UserUploads.
             user_key = self.key("UserUploads", username)
             user_uploads = self.client.get(user_key)
             if user_uploads:  # If user has uploaded pages previosly.
@@ -53,8 +49,7 @@ class Tracker:
                 new_user_upload.update({"uploads": [pagename]})
                 trans.put(new_user_upload)
 
-        # Add username to page uploader in PageUploader.
-        with self.client.transaction() as trans:
+            # Add username to page uploader in PageUploader.
             page_key = self.key("PageUploader", pagename)
             new_page_upload = datastore.Entity(key=page_key)
             new_page_upload.update({"uploader": username})
@@ -140,3 +135,52 @@ class Tracker:
         page_key = self.key("Upvote", pagename)
         page = self.client.get(page_key)
         return len(page["upvotes"]) if page else 0
+
+    def add_comment(self, pagename: str, username: str, comment: str) -> None:
+        """
+        Keeps track of comments left by different users on a page.
+
+        ---
+        Args:
+            pagename:
+                Sting representing username of the uploader.
+            username:
+                String containing the name of uploaded page.
+            comment:
+                String containing a user's comment.                 
+        """        
+        with self.client.transaction() as trans:
+            page_key = self.key("PageComment", pagename)
+            page = self.client.get(page_key)
+            if page:  # If page has been commented before.
+                page_comments = json.loads(str(page["comments"]).replace("\'", "\""))  # Converts database text to Python dictionary.
+                comment_num = str(len(page_comments))  # Define the number of the comment as a string.
+                page_comments[comment_num] = {username: comment.replace("'", "`").replace('"', "``")}  # Add comment to the page.
+                page["comments"] = str(page_comments)  # Cast the dictionary back to string.
+                trans.put(page)
+            else:  # Page is receiving its first comment.
+                new_page_comment = datastore.Entity(key=page_key)
+                new_page_comment.update({"comments": str({"0": {username: comment.replace("'", "`").replace('"', "``")}})})
+                trans.put(new_page_comment)
+        
+    def get_comments(self, pagename: str):
+        """
+        Get all comments left on page with parameter pagename.
+        
+        ---
+        Args:
+            pagename:
+                String containing the name of a wiki page.
+
+        Returns:
+            A dictionary containing key-value pairs of int (comment number)
+            and value (dictionary with username as key and comment as value)
+            of all comments left on the page.
+        """
+        page_key = self.key("PageComment", pagename)
+        page = self.client.get(page_key)
+        result = None
+        if page:
+            result = json.loads(str(page["comments"]).replace("\'", "\""))
+        return result
+        # return json.loads(str(page["comments"]).replace("\'", "\"")) if page else None                    
